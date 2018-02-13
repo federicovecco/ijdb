3bd1af65bd3a0af65bdb83eaf182140a9e6f8505
Federico Vecco <federico.vecco@gmail.com>
2018-02-13 13:47:22 +0000

    Adding classes and Controller

--- jokes.php
+++ jokes.php
@@ -2,13 +2,29 @@
 
 try {
 	include __DIR__ . '/includes/DatabaseConnection.php';
-	include __DIR__ . '/includes/DatabaseFunctions.php';
+	include __DIR__ . '/classes/DatabaseTable.php';
 
-	$jokes = allJokes($pdo);
+	$jokesTable = new DatabaseTable($pdo, 'joke', 'id');
+	$authorsTable = new DatabaseTable($pdo, 'author', 'id');
+
+	$result = $jokesTable->findAll();
+
+	$jokes = [];
+
+	foreach ($result as $joke) {
+		$author = $authorsTable->findById($joke['authorId']);
+		$jokes[] = [
+		'id' => $joke['id'],
+		'joketext' => $joke['joketext'],
+		'jokedate' => $joke['jokedate'],
+		'name' => $author['name'],
+		'email' => $author['email']
+		];
+	}
 
 	$title = 'Joke List';
 
-	$totalJokes = totalJokes($pdo);
+	$totalJokes = $jokesTable->total();
 
 	ob_start();
 	include __DIR__ . '/tmp/jokes.html.php';
